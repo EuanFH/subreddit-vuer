95f1db9d819b339a0a21b2c8eb3ccaf7d50f18fa
Euan Haahr <euanhaahrmail2@gmail.com>
2020-01-16 21:28:46 +0000

    started using scss and have set a global theme scss file

--- vue.config.js
+++ vue.config.js
@@ -1,18 +1,11 @@
 const path = require('path')
-
 module.exports = {
-  chainWebpack: config => {
-    const types = ['vue-modules', 'vue', 'normal-modules', 'normal']
-    types.forEach(type => addStyleResource(config.module.rule('sass').oneOf(type)))
-  },
-}
-
-function addStyleResource (rule) {
-  rule.use('style-resource')
-    .loader('style-resources-loader')
-    .options({
-      patterns: [
-        path.resolve(__dirname, './src/themes/dark.scss'),
-      ],
-    })
+  pluginOptions: {
+    'style-resources-loader': {
+      'preProcessor': 'scss',
+      'patterns': [
+        path.resolve(__dirname, './src/theme/dark.scss'),
+      ]
+    }
+  }
 }
