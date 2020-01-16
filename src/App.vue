1b384d4ccab5da7f4e3b3d20ae15833b5e3f3451
Euan Haahr <euanhaahrmail2@gmail.com>
2020-01-16 21:24:21 +0000

    added theme styles currently broke. started working on fab and removing example code

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Fab icon="+"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Fab from './components/Fab.vue' 
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Fab,
   }
 }
 </script>
---
+++ src/components/Fab.vue
@@ -0,0 +1,19 @@
+<template>
+    <button>{{icon}}</button>
+</template>
+
+<script>
+export default {
+    name: 'Fab',
+    props: {
+        icon: String //probably somthing else
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+button {
+    border-radius: 50%;
+    background-color: $primary-color;
+}
+</style>
