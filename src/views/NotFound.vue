5576bbf4591eba896e9d07a9f2738f3188f57fe3
Euan Haahr <euanhaahrmail2@gmail.com>
2020-01-19 17:42:32 +0000

    Finished creating and styling the page not found view

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,9 +1,33 @@
 <template>
-    <p>Not Found</p>
+    <main>
+        <RedditIcon class='RedditIcon'/>
+        <h1>Sorry!</h1>
+        <h2>The page you are looking for dosn't exist.</h2>
+    </main>
 </template>
 
 <script>
+import RedditIcon from '@/components/RedditIcon'
+
+export default {
+    name: 'NotFound',
+    components: {
+        RedditIcon
+    }
+}
 </script>
 
 <style lang="scss" scoped>
+main{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+    color: $secondary-color;
+}
+
+.RedditIcon{
+    max-width: 12.5em;
+}
 </style>
