35431622d00f1260b78321da9d0b8603865d3440
Euan Haahr <euanhaahrmail2@gmail.com>
2020-01-20 20:56:09 +0000

    couldnt get any router hacks to work for github pages just going to self host

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,5 @@
 const path = require('path')
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
-    ? "/subreddit-vuer"
-    : "/",
     outputDir: "docs",
     configureWebpack: {
         devtool: 'source-map'
